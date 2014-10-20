e4411b983ceaf3c11e661b3b797fea91bab38adb
Stephen Young <StephenJayYoung@gmail.com>
2014-10-20 10:38:38 -0700

    started working on the api

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -5,4 +5,8 @@ var Foursquareandember = Ember.Application.create();
 // expose Foursquareandember globally
 window.Foursquareandember = Foursquareandember;
 
+Yocaribou.Router.map(function() {
+	this.route('foursquareAPI', {path:'/foursquareAPI'})
+});
+
 
