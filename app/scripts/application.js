9829977bd88efd8dd1f86538959bad8de9eb1ab6
Stephen Young <StephenJayYoung@gmail.com>
2014-10-20 11:40:39 -0700

    the api route is now working

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -5,8 +5,12 @@ var Foursquareandember = Ember.Application.create();
 // expose Foursquareandember globally
 window.Foursquareandember = Foursquareandember;
 
-Yocaribou.Router.map(function() {
+Foursquareandember.Router.map(function() {
 	this.route('foursquareAPI', {path:'/foursquareAPI'})
 });
 
-
+Foursquareandember.foursquareAPIRoute = Ember.Route.extend({
+	model: function(){
+		return {APIstuff: 'oh, hi there ;) '}
+	}
+});
